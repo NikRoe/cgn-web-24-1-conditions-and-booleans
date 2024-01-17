eaeaeff0a8b260f25acdfc89cbefc0fe1a6dc1d7
Niklas Röttger <niklas.roettger@outlook.de>
2024-01-17 14:47:47 +0100

    add demo code

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
+console.clear();
+
 let carrots = 2;
-const dishesCleaned = 0;
+const dishedCleaned = 4;
+
+// Generating a boolean with the help of a comparison operator
+
+const kidDidHelpCleanTheKitchen = dishedCleaned === 3;
+
+// Ternary Operator
+
+let extraCarrots = kidDidHelpCleanTheKitchen ? 2 : 1;
+
+// If-Else Statement
+
+// if (kidDidHelpCleanTheKitchen) {
+//   extraCarrots = 2;
+// } else {
+//   extraCarrots = -1;
+// }
+
+// carrots = carrots + extraCarrots;
+
+// console.log(carrots);
+
+// Type coercion
+
+const newCarrots = "0";
+
+if (newCarrots) {
+  console.log("Hand out the carrots!!");
+} else {
+  console.log("Mein Wert war leider false");
+}
